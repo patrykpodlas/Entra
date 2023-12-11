8b0e8e22060499463aa10e011174f7b602e0beca
patryk.podlas <patryk.podlas@claranet.com>
2023-12-11 09:50:00 +0000

    v1.1

--- Compare-MgUserMemberships.ps1
+++ Compare-MgUserMemberships.ps1
@@ -37,34 +37,37 @@ function Compare-MgUserMemberships {
     )
 
     begin {
-        $userOneId = (Get-MgUser -Filter "UserPrincipalName eq '$($userOne)'").Id
-        $userOneMemberships = (Get-MgUserMemberOf -UserId $userOneId | Select-Object Id, AdditionalProperties)
-        $userOneMembershipsObject = @()
-        foreach ($item in $userOneMemberships) {
-            $customObject = [PSCustomObject]@{
-                displayName           = $item.AdditionalProperties.displayName
-                id                    = $item.Id
-                groupTypes            = $item.AdditionalProperties.groupTypes
-                onPremisesSyncEnabled = $item.AdditionalProperties.onPremisesSyncEnabled
+        try {
+            $userOneId = (Get-MgUser -Filter "UserPrincipalName eq '$($userOne)'").Id
+            $userOneMemberships = (Get-MgUserMemberOf -UserId $userOneId | Select-Object Id, AdditionalProperties)
+            $userOneMembershipsObject = @()
+            foreach ($item in $userOneMemberships) {
+                $customObject = [PSCustomObject]@{
+                    displayName           = $item.AdditionalProperties.displayName
+                    id                    = $item.Id
+                    groupTypes            = $item.AdditionalProperties.groupTypes
+                    onPremisesSyncEnabled = $item.AdditionalProperties.onPremisesSyncEnabled
+                }
+                $userOneMembershipsObject += $customObject
             }
-            $userOneMembershipsObject += $customObject
-        }
 
-        $userTwoId = (Get-MgUser -Filter "UserPrincipalName eq '$($userTwo)'").Id
-        $userTwoMemberships = (Get-MgUserMemberOf -UserId $userTwoId | Select-Object Id, AdditionalProperties)
-        $userTwoMembershipsObject = @()
-        foreach ($item in $userTwoMemberships) {
-            $customObject = [PSCustomObject]@{
-                displayName           = $item.AdditionalProperties.displayName
-                id                    = $item.Id
-                groupTypes            = $item.AdditionalProperties.groupTypes
-                onPremisesSyncEnabled = $item.AdditionalProperties.onPremisesSyncEnabled
+            $userTwoId = (Get-MgUser -Filter "UserPrincipalName eq '$($userTwo)'").Id
+            $userTwoMemberships = (Get-MgUserMemberOf -UserId $userTwoId | Select-Object Id, AdditionalProperties)
+            $userTwoMembershipsObject = @()
+            foreach ($item in $userTwoMemberships) {
+                $customObject = [PSCustomObject]@{
+                    displayName           = $item.AdditionalProperties.displayName
+                    id                    = $item.Id
+                    groupTypes            = $item.AdditionalProperties.groupTypes
+                    onPremisesSyncEnabled = $item.AdditionalProperties.onPremisesSyncEnabled
+                }
+                $userTwoMembershipsObject += $customObject
             }
-            $userTwoMembershipsObject += $customObject
+
+            $allMemberships = $userOneMembershipsObject + $userTwoMembershipsObject | Sort-Object displayName -Unique
+        } catch {
+            throw $_.Exception
         }
-
-        $allMemberships = $userOneMembershipsObject + $userTwoMembershipsObject | Sort-Object displayName -Unique
-
     }
 
     process {
@@ -92,34 +95,39 @@ function Compare-MgUserMemberships {
 
         if ($Copy) {
             $filteredGroups = $results | Where-Object { ($_.type -notcontains "DynamicMembership") -and ($_."$userTwo status" -eq "Not a member") -and ($_.onPremisesSyncEnabled -ne "True") }
-            Write-Host "--- You can only copy the following groups:" -ForegroundColor Yellow
-            Write-Output $filteredGroups | Format-Table
-            $readInput = Read-Host -Prompt "--- Do you want to copy all or through an index? (All/Index)"
-            if ($readInput -eq "All") {
-                Write-Host "--- Copying all group memberships from user $userOne to $userTwo" -ForegroundColor Yellow
-                # Filter for groups the user is not a member of and also the group is not of dynamic memership type.
-                foreach ($group in $filteredGroups) {
-                    Write-Host "    --- Adding $UserTwo to group $($group.groupName)" -ForegroundColor Yellow
-                    New-MgGroupMember -GroupId $group.id -DirectoryObjectId $userTwoId -WhatIf
-                }
-            } elseif ($readInput -eq "Index") {
-                [int]$readInput = Read-Host -Prompt "Specify index from the table"
-                if ($readInput -in $filteredGroups.Index) {
-                    $group = $($filteredGroups | Where-Object Index -eq $readInput | Select-Object Id, groupName)
-                    Write-Host "--- Adding $UserTwo to group '$($group.groupName)' with Id: $($group.id)" -ForegroundColor Yellow
-                    Get-MgGroup -GroupId $($group.id)
-                    New-MgGroupMember -GroupId $($group.id) -DirectoryObjectId $userTwoId
+            if ($null -ne $filteredGroups) {
+                Write-Host "--- You can only copy the following groups:" -ForegroundColor Yellow
+                Write-Output $filteredGroups | Format-Table
+                $readInput = Read-Host -Prompt "--- Do you want to copy all or through an index? (All/Index)"
+                if ($readInput -eq "All") {
+                    Write-Host "--- Copying all group memberships from user $userOne to $userTwo" -ForegroundColor Yellow
+                    # Filter for groups the user is not a member of and also the group is not of dynamic memership type.
+                    foreach ($group in $filteredGroups) {
+                        Write-Host "    --- Adding $UserTwo to group $($group.groupName)" -ForegroundColor Yellow
+                        New-MgGroupMember -GroupId $group.id -DirectoryObjectId $userTwoId -WhatIf
+                    }
+                } elseif ($readInput -eq "Index") {
+                    [int]$readInput = Read-Host -Prompt "Specify index from the table"
+                    if ($readInput -in $filteredGroups.Index) {
+                        $group = $($filteredGroups | Where-Object Index -eq $readInput | Select-Object Id, groupName)
+                        Write-Host "--- Adding $UserTwo to group '$($group.groupName)' with Id: $($group.id)" -ForegroundColor Yellow
+                        Get-MgGroup -GroupId $($group.id)
+                        New-MgGroupMember -GroupId $($group.id) -DirectoryObjectId $userTwoId
+                    } else {
+                        Write-Error "--- The user can't be added to the group(s), either the user is already a member of the group or the group is of dynamic membership type!"
+                    }
                 } else {
-                    Write-Error "--- The user can't be added to the group(s), either the user is already a member of the group or the group is of dynamic membership type!"
+                    Write-Error "You must specify either All or Index!"
+                    return
                 }
             } else {
-                Write-Error "You must specify either All or Index!"
-                return
+                Write-Host "--- There are no groups you can copy over." -ForegroundColor Yellow
+                Write-Host ""
             }
         }
     }
 
     end {
-
+        Write-Host "--- Finished all operations." -ForegroundColor Yellow
     }
 }
